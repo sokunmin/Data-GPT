fca3136654de5d4ce979a42ee9f839a0faa888db
Chun-Ming <sokunmin@gmail.com>
2023-07-24 07:40:03 +0800

    feat: add new version of Chroma

--- app.py
+++ app.py
@@ -1,15 +1,18 @@
-from os.path import basename
-
 import fitz
+import shared
 import gradio as gr
 import pandas as pd
+from shared import load_file
+from typing import List, Set
 from PIL import Image
 from dotenv import load_dotenv
+from os.path import basename, join, exists
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.vectorstores import Chroma
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 
-import shared
 
-
-def feed_csv_into_collector(files):
+def load_csv_to_table(files: List[str]):
     dfs = []
     for file in files:
         df = pd.read_csv(file.name)
@@ -18,11 +21,47 @@ def feed_csv_into_collector(files):
     return dfs
 
 
-def feed_pdf_into_collector(files):
+def on_csv_selected(df, evt: gr.SelectData):
+    assert isinstance(evt.target, gr.components.Dataframe)
+    row, col = evt.index
+    file_path = df['File path'][row]
+    docs = load_file(file_path)
+    if file_path not in shared.db['files']:
+        text_splitter = RecursiveCharacterTextSplitter(
+            chunk_size=shared.chunk_size,
+            chunk_overlap=shared.chunk_overlap
+        )
+        texts = text_splitter.split_documents(docs)
+        # create embeddings for the CSV
+        shared.db['db'].add_documents(texts)
+
+    return docs #pd.read_csv(file_path)
+
+
+def load_pdf_to_table(files: List[str]):
     pdf_list = []
     for file in files:
         pdf_list.append((basename(file.name), file.name))
     return pdf_list
+
+
+def on_pdf_selected(pdf, evt: gr.SelectData):
+    row, col = evt.index
+    pdf_path = pdf[row][1]
+    print(pdf_path)
+    image, page_count = render_pdf(pdf_path)
+    # create embeddings for the PDF
+    # TODO:
+    return image, gr.update(minimum=1, maximum=page_count, value=1)
+
+
+def render_pdf(pdf_path: str):
+    doc = fitz.open(pdf_path)
+    page = doc[0]
+    # Render the page as a PNG image with a resolution of 300 DPI
+    pix = page.get_pixmap(matrix=fitz.Matrix(300 / 150, 300 / 150))
+    image = Image.frombytes('RGB', [pix.width, pix.height], pix.samples)
+    return image, doc.page_count
 
 
 def ask_chatbot_questions(history, question):
@@ -34,30 +73,6 @@ def ask_chatbot_questions(history, question):
 
 def get_chatbot_response():
     pass
-
-
-def on_csv_selected(df, evt: gr.SelectData):
-    assert isinstance(evt.target, gr.components.Dataframe)
-    row, col = evt.index
-    file_path = df['File path'][row]
-    return pd.read_csv(file_path)
-
-
-def on_pdf_selected(pdf, evt: gr.SelectData):
-    row, col = evt.index
-    pdf_path = pdf[row][1]
-    print(pdf_path)
-    image, page_count = render_pdf(pdf_path)
-    return image, gr.update(minimum=1, maximum=page_count, value=1)
-
-
-def render_pdf(pdf_path):
-    doc = fitz.open(pdf_path)
-    page = doc[0]
-    # Render the page as a PNG image with a resolution of 300 DPI
-    pix = page.get_pixmap(matrix=fitz.Matrix(300 / 150, 300 / 150))
-    image = Image.frombytes('RGB', [pix.width, pix.height], pix.samples)
-    return image, doc.page_count
 
 
 def output_pdf_pages():
@@ -76,6 +91,34 @@ def output_prediction():
     pass
 
 
+def create_embeddings():
+    model_name = shared.embeddings_model_name
+    embeddings = HuggingFaceEmbeddings(model_name=model_name)
+    return embeddings
+
+
+def load_db():
+    persist_directory = shared.persist_directory
+    embeddings = create_embeddings()
+    if exists(join(persist_directory, 'chroma.sqlite3')):
+        # Update and store locally vectorstore
+        db = Chroma(
+            persist_directory=persist_directory,
+            embedding_function=embeddings,
+            client_settings=shared.CHROMA_SETTINGS
+        )
+        shared.db['db'] = db
+        collections = set([metadata['source'] for metadata in db.get()['metadatas']])
+        shared.db['files'] = collections
+    else:
+        shared.db = Chroma.from_documents(
+            [],
+            embedding=embeddings,
+            persist_directory=persist_directory,
+            client_settings=shared.CHROMA_SETTINGS
+        )
+
+
 def main():
     with gr.Blocks() as ui:
         with gr.Row().style(equal_height=True):
@@ -83,105 +126,106 @@ def main():
             with gr.Column(scale=5):
                 # PDF tab
                 with gr.Tab("PDF"):
-                    shared.gradio['pdf_button'] = gr.UploadButton(
+                    shared.gr['pdf_button'] = gr.UploadButton(
                         label="Upload PDFs", file_types=['.pdf'], file_count="multiple")
-                    shared.gradio['pdf_file_list'] = gr.Dataframe(
+                    shared.gr['pdf_file_list'] = gr.Dataframe(
                         headers=["Filename", "File path"],
                         type="array", value=[], interactive=False)
                     with gr.Accordion("Preview"):
-                        shared.gradio['pdf_preview'] = gr.Image(
+                        shared.gr['pdf_preview'] = gr.Image(
                             label='PDF', tool='select', elem_id='preview_box'
                         ).style(height=680)
-                    shared.gradio['pdf_slider'] = gr.Slider(
+                    shared.gr['pdf_slider'] = gr.Slider(
                         label='Page', step=1, interactive=True)
-                    shared.gradio['pdf_pages'] = gr.HighlightedText(
+                    shared.gr['pdf_pages'] = gr.HighlightedText(
                         label='Found at', combine_adjacent=True, show_legend=True
                     ).style(color_map={"Chapter": "green", "Page#": 'blue'})
                 # CSV tab
                 with gr.Tab("CSV"):
-                    shared.gradio['csv_button'] = gr.UploadButton(
+                    shared.gr['csv_button'] = gr.UploadButton(
                         label="Upload CSVs", file_types=['.csv'], file_count="multiple")
-                    shared.gradio['csv_file_list'] = gr.Dataframe(
+                    shared.gr['csv_file_list'] = gr.Dataframe(
                         headers=["Filename", "Categories", "File path"],
                         type="pandas", value=[], interactive=False)
                     with gr.Accordion("Drop here to preview"):
-                        shared.gradio['csv_preview'] = gr.DataFrame(
+                        shared.gr['csv_preview'] = gr.DataFrame(
                             overflow_row_behaviour='paginate', max_rows=12, interactive=True)
                 # SQL tab
                 with gr.Tab("SQL"):
-                    shared.gradio["query_textarea"] = gr.TextArea(placeholder="Enter the SQL query here ...")
-                    shared.gradio['query_button'] = gr.Button(value="Query")
+                    shared.gr["query_textarea"] = gr.TextArea(placeholder="Enter the SQL query here ...")
+                    shared.gr['query_button'] = gr.Button(value="Query")
                     with gr.Accordion("Query Result"):
-                        shared.gradio['query_dataframe'] = gr.DataFrame(headers=["Name", "Age", "Gender"], )
+                        shared.gr['query_dataframe'] = gr.DataFrame(headers=["Name", "Age", "Gender"], )
 
                 # Settings tab
                 with gr.Tab("Settings"):
                     gr.Markdown("## Model settings")
-                    shared.gradio['temp_slider'] = gr.Slider(0, 10, step=1, label="Temperature")
-                    shared.gradio['model_dropdown'] = gr.Dropdown(
+                    shared.gr['temp_slider'] = gr.Slider(0, 10, step=1, label="Temperature")
+                    shared.gr['model_dropdown'] = gr.Dropdown(
                         ["ChatGPT-3.5 turbo", "ChatGPT-4", "Bing", "Poe"],
                         label="Model", info="Choose the backend of chat bot")
                     gr.Markdown("***")  # horizontal line
                     gr.Markdown("## Database settings")  # horizontal line
-                    shared.gradio['db_url_textbox'] = gr.Textbox(label="URL", value="http://localhost:5432")
+                    shared.gr['db_url_textbox'] = gr.Textbox(label="URL", value="http://localhost:5432")
                     with gr.Row():
-                        shared.gradio['db_user_textbox'] = gr.Textbox(label="User", value="postgres")
-                        shared.gradio['db_passwd_textbox'] = gr.Textbox(
+                        shared.gr['db_user_textbox'] = gr.Textbox(label="User", value="postgres")
+                        shared.gr['db_passwd_textbox'] = gr.Textbox(
                             label="Password", type='password', value="postgres")
-                        shared.gradio['db_name_textbox'] = gr.Textbox(label="Database", value="postgres-db")
-                    shared.gradio['db_save_button'] = gr.Button(value="Test & Save")
-                shared.gradio['ask_textbox'] = gr.Textbox(
+                        shared.gr['db_name_textbox'] = gr.Textbox(label="Database", value="postgres-db")
+                    shared.gr['db_save_button'] = gr.Button(value="Test & Save")
+                shared.gr['ask_textbox'] = gr.Textbox(
                     label="Prompt", placeholder="Ask questions here ...", lines=3)
                 with gr.Row():
-                    shared.gradio['clear_button'] = gr.Button(value="Clear")
-                    shared.gradio['submit_button'] = gr.Button(value="Submit")
+                    shared.gr['clear_button'] = gr.Button(value="Clear")
+                    shared.gr['submit_button'] = gr.Button(value="Submit")
             # right panel
             with gr.Column(scale=5):
                 with gr.Tab("Chat"):
-                    shared.gradio['chatbot_history'] = gr.Chatbot(value=[])
+                    shared.gr['chatbot_history'] = gr.Chatbot(value=[])
                 with gr.Tab("Result"):
-                    shared.gradio['result_dataframe'] = gr.Dataframe(
+                    shared.gr['result_dataframe'] = gr.Dataframe(
                         label="Output", headers=["1", "2", "3"], max_rows=30)
-                    shared.gradio['result_timeseries'] = gr.Timeseries(
+                    shared.gr['result_timeseries'] = gr.Timeseries(
                         label="Chart", x="time", y=['retail', 'food', 'other'])
-                    shared.gradio['result_label'] = gr.Label(label="Prediction")
+                    shared.gr['result_label'] = gr.Label(label="Prediction")
                 with gr.Tab("History"):
-                    shared.gradio['history_label'] = gr.Label()
+                    shared.gr['history_label'] = gr.Label()
                 with gr.Tab("Debug Msg"):
-                    shared.gradio['debug_label'] = gr.Label()
-        shared.gradio['pdf_button'].upload(
-            fn=feed_pdf_into_collector,
-            inputs=shared.gradio['pdf_button'],
-            outputs=shared.gradio['pdf_file_list'],
+                    shared.gr['debug_label'] = gr.Label()
+        shared.gr['pdf_button'].upload(
+            fn=load_pdf_to_table,
+            inputs=shared.gr['pdf_button'],
+            outputs=shared.gr['pdf_file_list'],
             api_name='upload_pdf'
         )
-        shared.gradio['pdf_file_list'].select(
+        shared.gr['pdf_file_list'].select(
             fn=on_pdf_selected,
-            inputs=shared.gradio['pdf_file_list'],
-            outputs=[shared.gradio['pdf_preview'], shared.gradio['pdf_slider']]
+            inputs=shared.gr['pdf_file_list'],
+            outputs=[shared.gr['pdf_preview'], shared.gr['pdf_slider']]
         )
-        shared.gradio['csv_button'].upload(
-            fn=feed_csv_into_collector,
-            inputs=shared.gradio['csv_button'],
-            outputs=shared.gradio['csv_file_list'],
+        shared.gr['csv_button'].upload(
+            fn=load_csv_to_table,
+            inputs=shared.gr['csv_button'],
+            outputs=shared.gr['csv_file_list'],
             api_name="upload_csv")
-        shared.gradio['csv_file_list'].select(
+        shared.gr['csv_file_list'].select(
             fn=on_csv_selected,
-            inputs=shared.gradio['csv_file_list'],
-            outputs=shared.gradio['csv_preview'])
-        shared.gradio['submit_button'].click(
+            inputs=shared.gr['csv_file_list'],
+            outputs=shared.gr['csv_preview'])
+        shared.gr['submit_button'].click(
             fn=ask_chatbot_questions,
-            inputs=[shared.gradio['chatbot_history'], shared.gradio['ask_textbox']],
-            outputs=[shared.gradio['chatbot_history']],
+            inputs=[shared.gr['chatbot_history'], shared.gr['ask_textbox']],
+            outputs=[shared.gr['chatbot_history']],
             queue=False
         ).success(
             fn=get_chatbot_response,
-            inputs=[shared.gradio['chatbot_history'], shared.gradio['ask_textbox'], llm],
-            outputs=[shared.gradio['chatbot_history']]
+            inputs=[shared.gr['chatbot_history'], shared.gr['ask_textbox']],
+            # inputs=[shared.gr['chatbot_history'], shared.gr['ask_textbox'], llm],
+            outputs=[shared.gr['chatbot_history']]
         ).success(
             fn=render_pdf,
             inputs=[],
-            outputs=[shared.gradio['pdf_preview'], shared.gradio['pdf_slider'], shared.gradio['pdf_pages']]
+            outputs=[shared.gr['pdf_preview'], shared.gr['pdf_slider'], shared.gr['pdf_pages']]
         )
 
     ui.launch()
@@ -189,4 +233,5 @@ def main():
 
 if __name__ == '__main__':
     load_dotenv()
+    load_db()
     main()
--- shared.py
+++ shared.py
@@ -1,3 +1,58 @@
+import os
+from typing import List
+from langchain.document_loaders import (
+    CSVLoader,
+    PyMuPDFLoader,
+    TextLoader,
+    UnstructuredEPubLoader,
+    UnstructuredHTMLLoader,
+    UnstructuredMarkdownLoader,
+    UnstructuredODTLoader,
+)
+from chromadb.config import Settings
+from langchain.schema import Document
+
+# Define the folder for storing database
+persist_directory = os.environ.get('PERSIST_DIRECTORY')
+source_directory = os.environ.get('SOURCE_DIRECTORY', 'source_documents')
+embeddings_model_name = os.environ.get('EMBEDDINGS_MODEL_NAME')
+chunk_size = 500
+chunk_overlap = 50
 
 
-gradio = {}
+# Define the Chroma settings
+CHROMA_SETTINGS = Settings(
+        persist_directory=persist_directory,
+        anonymized_telemetry=False
+)
+
+gr = {}
+
+db = {
+    'db': None,
+    'files': set()
+}
+
+
+# Map file extensions to document loaders and their arguments
+FILE_MAPPING = {
+    ".csv": (CSVLoader, {}),
+    ".epub": (UnstructuredEPubLoader, {}),
+    ".html": (UnstructuredHTMLLoader, {}),
+    ".md": (UnstructuredMarkdownLoader, {}),
+    ".odt": (UnstructuredODTLoader, {}),
+    ".pdf": (PyMuPDFLoader, {}),
+    ".txt": (TextLoader, {"encoding": "utf8"}),
+    # Add more mappings for other file extensions and loaders as needed
+}
+
+
+def load_file(file_path: str) -> List[Document]:
+    ext = "." + file_path.rsplit(".", 1)[-1]
+    if ext in FILE_MAPPING:
+        loader_class, loader_args = FILE_MAPPING[ext]
+        loader = loader_class(file_path, **loader_args)
+        return loader.load()
+
+    raise ValueError(f"Unsupported file extension '{ext}'")
+
